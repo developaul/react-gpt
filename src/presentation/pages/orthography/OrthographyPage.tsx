b2cc905ec90539b6f53a5aff72adef1e9f00be43
Paul Chavez <paulchavezromero20@gmail.com>
2024-06-03 21:48:26 -0500

    chore: chat bubbles added

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,5 +1,19 @@
+import { GptMessage, MyMessage } from "../../components"
+
 export const OrthographyPage = () => {
   return (
-    <div>OrthographyPage</div>
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+
+
+          {/* Bienvenida */}
+          <GptMessage text="Hola, puedes escribir tu texto en español, y te ayudo con las correcciones" />
+
+          <MyMessage text="Hola, ¿cómo estás?" />
+
+        </div>
+      </div>
+    </div>
   )
 }
